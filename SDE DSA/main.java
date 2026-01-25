a1f422520249f05c559de2710c1f1c27cc45f781
Vidhan Tomar <112256615+deathlyhollows4@users.noreply.github.com>
2026-01-25 07:26:29 +0000

    25 jan 2025

--- SDE DSA/main.java
+++ SDE DSA/main.java
@@ -5,12 +5,19 @@ public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     
     int row = sc.nextInt();
-    int col = sc.nextInt();
-    for (int i = 1; i <= row; i++) {
-        for(int j = row-i; j>=0; j--){
-            System.out.print(j+1);
+   // int col = sc.nextInt();
+    int nsp= row-1;
+    int nst=1;
+    for (int i = 1; i <= 2*row-1; i++) {
+        int k = 1;
+        for(int j = 1; j<= 2*row-1; j++){
+        if(i<j) System.out.print(i+" ");
+        else System.out.print(j+" ");
         }
+        
         System.out.println();
     }
+    
+    System.out.println("done");
 
 }
